68408b393c5cd56bb94f385c89c45224cbb658c9
Austin Wright <aaa@bzfx.net>
2018-06-17 20:14:56 -0700

    Add index.js
    oops

---
+++ index.js
@@ -0,0 +1,9 @@
+
+exports.tokenize = require('./lib/core.js').tokenize;
+exports.parse = require('./lib/core.js').parse;
+exports.inherits = require('./lib/core.js').inherits;
+exports.RDFaContext = require('./lib/core.js').RDFaContext;
+exports.RDFaParser = require('./lib/core.js').RDFaParser;
+
+exports.RDFaCoreParser = require('./lib/core.js').RDFaCoreParser;
+exports.RDFaXMLParser = require('./lib/host-xml.js').RDFaXMLParser;
